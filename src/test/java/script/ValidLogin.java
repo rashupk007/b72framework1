d8f5a7e4a95ce0e15114e56baed8ebf3c49d2216
rashupk007 <ahmedrashaad@hotmail.com>
2023-05-17 03:24:34 +0530

    Prepared testdata for ValidLogin Testcase updated ValidLogin class with code to get test data from excel file

--- src/test/java/script/ValidLogin.java
+++ src/test/java/script/ValidLogin.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
+import generic.Excel;
 import page.EnterTimeTrackPage;
 import page.LoginPage;
 
@@ -12,18 +13,22 @@ public class ValidLogin extends BaseTest {
 	@Test(priority = 1)
 	public void testValidLogin() {
 		
+		String un = Excel.getData(XL_PATH, "ValidLogin", 1, 0);
+		String pw = Excel.getData(XL_PATH, "ValidLogin", 1, 1);
+		
 		LoginPage loginPage=new LoginPage(driver);
 		EnterTimeTrackPage homePage=new EnterTimeTrackPage(driver);
 		
 //		1.enter valid username
-		loginPage.setUserName("admin");
+		loginPage.setUserName(un);
 //		2.enter valid password
-		loginPage.setPassword("manager");
+		loginPage.setPassword(pw);
 //		3.click login button
 		loginPage.clickLoginButton();
 //		4.verify home page is displayed or not
 		boolean result = homePage.verifyHomePageIsDisplayed(wait);
 		Assert.assertTrue(result);
+		
 	}
 
 }
