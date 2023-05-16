f0f77e01e1be8b822e8be0ccd470327e3172d454
rashupk007 <ahmedrashaad@hotmail.com>
2023-05-17 02:09:30 +0530

    Deleted GooglePage Added LoginPage

--- src/test/java/script/DemoTest.java
+++ src/test/java/script/DemoTest.java
@@ -4,21 +4,16 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 import generic.BaseTest;
 import generic.Excel;
-import page.GooglePage;
 
 public class DemoTest extends BaseTest {
 	
 	@Test
-	public void demoTest1() throws InterruptedException {
+	public void demoTest1() {
 		
 		String data = Excel.getData(XL_PATH, "sheet1", 1, 0);
 		Reporter.log("Test Data: "+data,true);
 		Reporter.log(driver.getTitle(),true);
 		
-		GooglePage g=new GooglePage(driver);
-		g.setSearch(data);
-		
-		Thread.sleep(1000);
 		
 	}
 
