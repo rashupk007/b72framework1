65fcefebc4078e8c1f00927641eacac8447bdcd5
rashupk007 <ahmedrashaad@hotmail.com>
2023-05-16 16:43:34 +0530

    Draft Copy

---
+++ src/test/java/script/DemoTest.java
@@ -0,0 +1,25 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import generic.BaseTest;
+import generic.Excel;
+import page.GooglePage;
+
+public class DemoTest extends BaseTest {
+	
+	@Test
+	public void demoTest1() throws InterruptedException {
+		
+		String data = Excel.getData(XL_PATH, "sheet1", 1, 0);
+		Reporter.log("Test Data: "+data,true);
+		Reporter.log(driver.getTitle(),true);
+		
+		GooglePage g=new GooglePage(driver);
+		g.setSearch(data);
+		
+		Thread.sleep(1000);
+		
+	}
+
+}
