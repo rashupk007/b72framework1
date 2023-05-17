9f6d9f8b81e919ecea57dc26eabbf7db4311577f
rashupk007 <ahmedrashaad@hotmail.com>
2023-05-17 23:26:27 +0530

    Added Error Message Element in LoginPage pom class.

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 
 public class LoginPage {
 	
@@ -15,6 +18,9 @@ public class LoginPage {
 	
 	@FindBy(xpath="//div[text()='Login ']")
 	private WebElement loginBTN;
+	
+	@FindBy(xpath="//span[contains(text(),'invalid')]")
+	private WebElement errMsg;
 	
 	public LoginPage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
@@ -30,6 +36,17 @@ public class LoginPage {
 	
 	public void clickLoginButton() {
 		loginBTN.click();
+	}
+	
+	public boolean verifyErrMsgIsDisplayed(WebDriverWait wait) {
+		try {
+			wait.until(ExpectedConditions.visibilityOf(errMsg));
+			Reporter.log("Err Msg is Displayed",true);
+			return true;
+		} catch (Exception e) {
+			Reporter.log("Err Msg is Displayed",true);
+			return false;
+		}
 	}
 	
 }
