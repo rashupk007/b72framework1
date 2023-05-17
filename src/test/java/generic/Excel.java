9512b9c6cd5430d0289668d3711b0dcfa8f91c6e
rashupk007 <ahmedrashaad@hotmail.com>
2023-05-17 23:47:58 +0530

    Added getRowCount method for executing test as per multiple rows of data

--- src/test/java/generic/Excel.java
+++ src/test/java/generic/Excel.java
@@ -6,7 +6,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class Excel {
 	
-public static String getData(String path, String sheet, int r, int c) {
+	public static String getData(String path, String sheet, int r, int c) {
 		
 		String value="";
 		try {
@@ -18,6 +18,21 @@ public static String getData(String path, String sheet, int r, int c) {
 		}
 		
 		return value;
+		
+	}
+	
+	public static int getRowCount(String path, String sheet) {
+		
+		int rc=0;
+		
+		try {
+			Workbook wb = WorkbookFactory.create(new FileInputStream(path));
+			rc=wb.getSheet(sheet).getLastRowNum();
+		} catch (Exception e) {
+			
+		}
+		
+		return rc;
 		
 	}
 	
