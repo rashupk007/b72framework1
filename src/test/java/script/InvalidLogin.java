4de30e1445cf9de0a9c72591f92b7895bdf329fc
rashupk007 <ahmedrashaad@hotmail.com>
2023-05-17 23:49:56 +0530

    Added code to run test multiple times based on test data sheet rows

--- src/test/java/script/InvalidLogin.java
+++ src/test/java/script/InvalidLogin.java
@@ -11,20 +11,24 @@ public class InvalidLogin extends BaseTest {
 	@Test(priority = 2)
 	public void testInvalidLogin() {
 		
-		String un = Excel.getData(XL_PATH, "ValidLogin", 1, 0);
-		String pw = Excel.getData(XL_PATH, "InvalidLogin", 1, 1);
-//		
-		LoginPage loginPage=new LoginPage(driver);
+		int rc=Excel.getRowCount(XL_PATH, "InvalidLogin");
 		
-//		1.enter valid username
-		loginPage.setUserName(un);
-//		2.enter valid password
-		loginPage.setPassword(pw);
-//		3.click login button
-		loginPage.clickLoginButton();
-//		4.verify error message is displayed or not
-		boolean result = loginPage.verifyErrMsgIsDisplayed(wait);
-		Assert.assertTrue(result);
+		for (int i=1;i<=rc;i++) {
+			String un = Excel.getData(XL_PATH, "ValidLogin", i, 0);
+			String pw = Excel.getData(XL_PATH, "InvalidLogin", i, 1);
+//			
+			LoginPage loginPage=new LoginPage(driver);
+			
+//			1.enter valid username
+			loginPage.setUserName(un);
+//			2.enter valid password
+			loginPage.setPassword(pw);
+//			3.click login button
+			loginPage.clickLoginButton();
+//			4.verify error message is displayed or not
+			boolean result = loginPage.verifyErrMsgIsDisplayed(wait);
+			Assert.assertTrue(result);
+		}
 		
 	}
 	
